a740ee2711c08b1a2821bb753e182c83fc70e33c
olga-kuriatnyk <olhak@uw.edu>
2022-03-24 21:35:27 -0700

    add sign up, log in and log out

--- app/src/main/java/com/example/insta/LoginActivity.kt
+++ app/src/main/java/com/example/insta/LoginActivity.kt
@@ -1,11 +1,77 @@
 package com.example.insta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // check if there's a user logged in
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
+        // if there is, take to MainActivity
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(username, password)
+        }
+
+        findViewById<Button>(R.id.signup_button).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            signUpUser(username, password)
+        }
+    }
+
+    private fun signUpUser(username: String, password: String) {
+        // Create the ParseUser
+        val user = ParseUser()
+
+        // Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // user has successfully created a new account
+                Log.i(TAG, "Successfully signed up in user")
+                goToMainActivity()
+            } else {
+                Toast.makeText(this, "Error signing in", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG, "Successfully logged in user")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error login in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        val TAG = "LoginActivity"
     }
 }
--- app/src/main/java/com/example/insta/MainActivity.kt
+++ app/src/main/java/com/example/insta/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.insta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.parse.ParseUser
 
 /*
  * Let user create a post by taking a photo with their camera
@@ -11,5 +16,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.logout_button).setOnClickListener {
+            ParseUser.logOut()
+            val currentUser = ParseUser.getCurrentUser() // this will now be null
+            Log.i(LoginActivity.TAG, "Successfully logged out")
+            goToLoginActivity()
+        }
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
